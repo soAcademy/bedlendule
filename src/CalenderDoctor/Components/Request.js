575662c46cf55aeae2b4e744bc86f46f53188237
Thanapon Bunchot <116933922+ThanaponBunchot@users.noreply.github.com>
2023-03-20 22:59:46 +0700

    add summit button for creating object of meeting data

--- src/CalenderDoctor/Components/Request.js
+++ src/CalenderDoctor/Components/Request.js
@@ -2,21 +2,21 @@ import { Calendar } from "primereact/calendar";
 import { useState, useEffect } from "react";
 
 const Request = ({ page, setPage }) => {
-  const [fromTime, setFromTime] = useState("15:00");
-  const [toTime, setToTime] = useState("16:00");
+  const [fromTime, setFromTime] = useState(new Date());
+  const [toTime, setToTime] = useState(new Date());
   const [date, setDate] = useState("20/03/2023");
   const [contact, setContact] = useState("093-xxx-xxxx");
   const [hourRate, setHourRate] = useState(1000);
   const [text, setText] = useState("");
-  console.log("fromtime", fromTime);
-  console.log("totime", toTime);
-  console.log("date", date);
-  console.log("contact", contact);
-  console.log("hourRate", hourRate);
-  console.log("text", text);
+  const [location, setLocation] = useState("Online");
+  const [request, setRequest] = useState([]);
+
+  const fromTimeOnly = fromTime.toLocaleTimeString().substring(0, 5);
+  const toTimeOnly = toTime.toLocaleTimeString().substring(0, 5);
+
   return (
     <>
-      <div className=" mx-auto mt-[70px] mb-5 w-[90%] rounded-lg  pb-5 shadow-xl  ">
+      <form className=" mx-auto mt-[70px] mb-5 w-[90%] rounded-lg  pb-5 shadow-xl  ">
         <div
           className=" px-2  text-right text-xl text-slate-500"
           onClick={() => setPage("patientSchedule")}
@@ -79,10 +79,16 @@ const Request = ({ page, setPage }) => {
         </div>
         <div className="mx-auto flex  p-2">
           <div className="  mx-auto space-x-8 text-sm text-white">
-            <button className="rounded-lg bg-primary  px-2 py-1 drop-shadow-lg">
+            <button
+              className="rounded-lg bg-primary  px-2 py-1 drop-shadow-lg hover:bg-teal-300 active:bg-teal-200"
+              onClick={() => setLocation("Online")}
+            >
               Online
             </button>
-            <button className="rounded-lg bg-secondary px-2  py-1 drop-shadow-lg">
+            <button
+              className="rounded-lg bg-secondary px-2  py-1 drop-shadow-lg"
+              onClick={() => setLocation("Offline")}
+            >
               Offline
             </button>
           </div>
@@ -94,16 +100,40 @@ const Request = ({ page, setPage }) => {
               placeholder="   Contact"
               onChange={(e) => setContact(e.target.value)}
               type="number"
+              required
             />
             <input
               className="h-[40px] w-1/2 rounded-lg border-2 border-slate-500 "
               placeholder="   Hour rate"
               onChange={(e) => setHourRate(e.target.value)}
               type="number"
+              required
             />
           </div>
         </div>
-      </div>
+        <div className="mx-auto  mt-5 w-full p-2">
+          <div className=" mx-auto  w-2/3 p-2">
+            <div className="mx-auto bg-yellow-100">
+              <div
+                className=" w-full rounded-lg bg-primary px-4 py-2 text-center text-white shadow-lg hover:bg-teal-300 active:bg-teal-200"
+                type="submit"
+                onClick={() =>
+                  setRequest({
+                    date: date.toLocaleDateString(),
+                    time: `${fromTimeOnly} - ${toTimeOnly}`,
+                    cause: text,
+                    contact: contact,
+                    hourRate: hourRate,
+                    locations: location,
+                  })
+                }
+              >
+                Summit
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
     </>
   );
 };
