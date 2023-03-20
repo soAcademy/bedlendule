3f430a3aa22d5fba4fbd6870694c2c7335a86104
Thanapon Bunchot <116933922+ThanaponBunchot@users.noreply.github.com>
2023-03-20 14:08:01 +0700

    Create Request page add time selction and add primary&secondary color

--- src/CalenderDoctor/Components/Request.js
+++ src/CalenderDoctor/Components/Request.js
@@ -1,40 +1,37 @@
-import ConfirmPopup from "./ConfirmPopup";
-import { MdClose } from "react-icons/md";
-
-const Request = ({ setConfirmPopupToggle, confirmPopupToggle, setPage }) => {
+import { FaClock } from "react-icons/fa";
+import { AiOutlineSchedule } from "react-icons/ai";
+const Request = ({ setPage }) => {
+  const timeRange = ["0:00", "1:00", "2:00", "3:00", "4:00", "5:00", "6:00"];
   return (
     <>
-      <div className="min-h-11/12 relative mx-auto mt-[70px] w-[95%] rounded-lg bg-white p-6 font-kanit shadow-xl">
-        <MdClose
-          className="absolute right-4 cursor-pointer text-2xl text-slate-400 duration-150 hover:text-slate-300"
-          onClick={() => setPage("patientSchedule")}
-        />
-        <p className="pt-4 text-center text-3xl font-bold text-slate-500">
-          CREATE REQUEST
-        </p>
-        <div className="flex">
-          <button
-            onClick={() => setConfirmPopupToggle(true)}
-            className="mx-auto rounded-md bg-[#99B47B] p-2 px-4 text-xs text-white duration-200 hover:bg-[#99b47bc8] active:bg-[#9cb482]"
-          >
-            CREATE REQUEST
-          </button>
+      <div className=" mx-auto mt-[70px] mb-5 h-screen w-[90%] rounded-lg bg-slate-100 shadow-lg">
+        <div
+          className=" px-2  text-right text-xl text-slate-500"
+          onClick={() => setPage("landing")}
+        >
+          x
+        </div>
+        <div className="flex text-center  text-3xl ">
+          <div className="w-full font-bold text-slate-500">REQUEST</div>
+        </div>
+        <div className=" mx-auto my-4 flex gap-3  text-center text-slate-400 ">
+          <div className="mx-auto flex gap-4">
+            <div className="flex gap-2  rounded-lg border-2 border-slate-500 py-3 px-2 relative">
+              <div className="absolute top-[-15px] bg-white px-1">DATE</div>
+              20/03/2023
+              <AiOutlineSchedule className="my-auto text-xl " />
+            </div>
+            <div className="flex gap-2  rounded-lg border-2 border-slate-500 py-3 px-2">
+              15:00
+              <FaClock className="my-auto" />
+            </div>
+            <div className="flex gap-2  rounded-lg border-2 border-slate-500 py-3 px-2">
+              16:00
+              <FaClock className="my-auto" />
+            </div>
+          </div>
         </div>
       </div>
-      {
-        <div
-          className={`fixed top-0 left-0 right-0 z-50 h-full w-full 
-        bg-slate-300 bg-opacity-10 backdrop-blur-[2px]
-        ${!confirmPopupToggle ? "scale-0" : "scale-1"}`}
-        >
-          <ConfirmPopup
-            title={"CREATE REQUEST"}
-            description={"Do you want to create request?"}
-            setConfirmPopupToggle={setConfirmPopupToggle}
-            confirmPopupToggle={confirmPopupToggle}
-          />
-        </div>
-      }
     </>
   );
 };
