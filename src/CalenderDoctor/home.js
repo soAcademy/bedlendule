17796346e14601c8c1e7b9f517e9624c02eed572
Bond Rungrot <st122150@ait.asia>
2023-03-23 11:14:29 +0700

    init

--- src/CalenderDoctor/home.js
+++ src/CalenderDoctor/home.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { Button, Checkbox } from "primereact/button";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 
 function Registration() {
   return (
@@ -46,6 +50,17 @@ function Registration() {
         <div className="flex flex-wrap justify-center">
           <div className="flex h-[45px] w-3/5 items-center rounded-lg border-2 border-slate-500">
             <input
+              type="email"
+              className="mx-2 h-[41px] w-full rounded-lg"
+              id="password"
+              placeholder="Email Address"
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-wrap justify-center">
+          <div className="my-2 flex h-[45px] w-3/5 items-center rounded-lg border-2 border-slate-500">
+            <input
               type="text"
               className="mx-2 h-[41px] w-full rounded-lg"
               id="username"
@@ -55,7 +70,7 @@ function Registration() {
         </div>
 
         <div className="flex flex-wrap justify-center">
-          <div className="my-2 flex h-[45px] w-3/5 items-center rounded-lg border-2 border-slate-500">
+          <div className="flex h-[45px] w-3/5 items-center rounded-lg border-2 border-slate-500">
             <input
               type="password"
               className="mx-2 h-[41px] w-full rounded-lg"
@@ -66,7 +81,7 @@ function Registration() {
         </div>
 
         <div className="flex flex-wrap justify-center">
-          <div className="flex h-[45px] w-3/5 items-center rounded-lg border-2 border-slate-500">
+          <div className="my-2 flex h-[45px] w-3/5 items-center rounded-lg border-2 border-slate-500">
             <input
               type="password"
               className="mx-2 h-[41px] w-full rounded-lg"
@@ -76,53 +91,51 @@ function Registration() {
           </div>
         </div>
 
-
         <div className="my-4 flex flex-wrap justify-center">
-          <div className="h-[45px] items-center rounded-lg">
-          {/* <label htmlFor="account-type">Account Type</label> */}
-          <div>
-            <label className="radio-inline">
-              <input
-                type="radio"
-                name="account-type"
-                id="psychologist"
-                value="psychologist"
-              />{" "}
-              Psychologist
-            </label>
-            <label className="mx-2 radio-inline">
-              <input
-                type="radio"
-                name="account-type"
-                id="client"
-                value="client"
-              />{" "}
-              Client
-            </label>
+          <div className="items-center rounded-lg">
+            {/* <label htmlFor="account-type">Account Type</label> */}
+            <div>
+              <label className="radio-inline">
+                <input
+                  type="radio"
+                  name="account-type"
+                  id="psychologist"
+                  value="psychologist"
+                />{" "}
+                Psychologist
+              </label>
+              <label className="radio-inline mx-2">
+                <input
+                  type="radio"
+                  name="account-type"
+                  id="client"
+                  value="client"
+                />{" "}
+                Client
+              </label>
+            </div>
           </div>
         </div>
-        </div>
 
-
-
-
-
-        <div className="form-group">
-          <div className="checkbox">
+        <div className="flex flex-wrap justify-center">
+          <div className="flex h-[40px] w-3/5 items-center rounded-lg">
             <label>
               <input type="checkbox" /> By registering, you are agreeing to our
               Terms of use and Privacy Policy
             </label>
           </div>
         </div>
-        <button type="submit" className="btn btn-primary">
-          Register
-        </button>
+
+        <div className="my-4 flex flex-wrap justify-center">
+          <Button label="Register" icon="pi pi-check" iconPos="right" />
+        </div>
       </form>
       <div className="already-have-account">
-        <p>
-          Already have an account? <a href="#">Login</a>
-        </p>
+        <div className="my-4 flex flex-wrap justify-center">
+          <p>
+            Already have an account? <a href="#">Login</a>
+          </p>
+        </div>
       </div>
     </div>
   );
