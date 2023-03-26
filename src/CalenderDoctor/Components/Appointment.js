bd47c2163f7a96a2e5900dc7227b24e584fef50e
Thanapon Bunchot <116933922+ThanaponBunchot@users.noreply.github.com>
2023-03-26 21:53:53 +0700

    creat Appointment popup

--- src/CalenderDoctor/Components/Appointment.js
+++ src/CalenderDoctor/Components/Appointment.js
@@ -1,8 +1,72 @@
-const Appointment = ()=>{
-  return <>
-  <div className="h-1/2 w-[80%] bg-red-200">
-    <div>APPPOINTMENT</div>
-  </div>
-  </>
+import { AiFillDollarCircle } from "react-icons/ai";
+import { GiAlarmClock } from "react-icons/gi";
+
+const Appointment = ({ setInsidePage, chooseTimeSlot }) => {
+  console.log("chooseTimeSlot>>>", chooseTimeSlot);
+  const description =
+    "You may not feel like it right now but thats my job. I want to hear every bit of what's going wrong and how it's impacting you now. At the same time we'll work on developing your belief in yourself in order to actually use the coping skills you probably already have.";
+
+  return (
+    <>
+      <div className="fixed top-[20%] right-[5%] w-[90%] rounded-lg p-2 shadow-lg ">
+        <div className="relative flex flex-col text-center">
+          <div className=" my-2 text-2xl font-bold">APPPOINTMENT</div>
+          <div
+            className="absolute right-0 p-2 text-xl opacity-50"
+            onClick={() => setInsidePage("selectDoctorDetail")}
+          >
+            x
+          </div>
+          <div className=" text-xl ">Patient : Jone Smith</div>
+          <div className="text-slate-700">Doctor : Marry Wickage</div>
+          <ul className="mx-auto my-4 flex cursor-pointer flex-row gap-2 hover:bg-[#C5E1A5] ">
+            <li className="  relative flex w-[25%] rounded-lg border-2 border-slate-400 p-2 text-center text-sm ">
+              <div className="absolute top-[-10px] w-[50px] bg-white px-1 text-slate-400">
+                From
+              </div>
+              <div className="mx-auto flex">
+                <div className="underline underline-offset-2 ">
+                  {chooseTimeSlot.startTime}
+                </div>
+                <div className="px-1">
+                  <GiAlarmClock className="text-base" />
+                </div>
+              </div>
+            </li>
+            <li className="  relative flex w-[25%]  rounded-lg border-2 border-slate-400 p-2 text-center text-sm">
+              <div className="absolute top-[-10px] bg-white px-1 text-slate-400">
+                To
+              </div>
+              <div className="mx-auto flex">
+                <div className="underline underline-offset-2">
+                  {chooseTimeSlot.FinishTime}
+                </div>
+                <div className="px-1">
+                  <GiAlarmClock className="text-base" />
+                </div>
+              </div>
+            </li>
+            <li className=" relative flex w-[50%]  rounded-lg border-2 border-slate-400 p-2 text-center text-sm">
+              <div className="absolute top-[-10px] bg-white px-1 text-slate-400">
+                Rate
+              </div>
+              <div className=" mx-auto flex">
+                <div className="underline underline-offset-2">
+                  {chooseTimeSlot.rate} THB/Hour
+                </div>
+                <div className="px-1">
+                  <AiFillDollarCircle className="my-auto text-base" />
+                </div>
+              </div>
+            </li>
+          </ul>
+          <div className="mx-3 rounded-lg border-2 border-slate-400 p-2 text-left text-sm text-slate-500">
+            &nbsp;&nbsp;&nbsp;{description}
+          </div>
+          <button className="button my-2 mx-auto w-[50%] py-2">CONFIRM</button>
+        </div>
+      </div>
+    </>
+  );
 };
 export default Appointment;
--- src/CalenderDoctor/Components/SelectDoctor.js
+++ src/CalenderDoctor/Components/SelectDoctor.js
@@ -5,6 +5,7 @@ import { ProgressSpinner } from "primereact/progressspinner";
 import SelectDoctorDetail from "./SelectedDoctorDetail";
 import { IoIosReturnLeft } from "react-icons/io";
 import { Calendar } from "primereact/calendar";
+import Appointment from "./Appointment";
 
 const SelectDoctor = ({
   date,
@@ -15,8 +16,9 @@ const SelectDoctor = ({
 }) => {
   const [doctors, setDoctors] = useState([]);
   const [selectedDoctor, setSelectedDoctor] = useState([]);
-  const [page, setPage] = useState("doctorLists");
+  const [page, setPage] = useState("doctorLists"); // อย่าลืมเปลี่ยน doctorLists
   const [fetching, setFetching] = useState(false);
+
   console.log("selectedDoctor", selectedDoctor);
   console.log("doctors", doctors);
   console.log("page", page);
@@ -65,87 +67,87 @@ const SelectDoctor = ({
 
   return (
     <>
-      {/* {page === "doctorLists" && ( */}
-        <div
-          className={`mt-4 p-2 duration-200 ${
-            page === "doctorLists" ? "opacity-100" : "opacity-0 pointer-events-none"
-          } `}
+      <div
+        className={`mt-4 p-2 duration-200 ${
+          page === "doctorLists"
+            ? "opacity-100"
+            : "pointer-events-none opacity-0"
+        } `}
+      >
+        <button
+          className="fixed right-5 z-40 w-10 rounded-lg border px-1 text-2xl font-light text-slate-400 shadow-md hover:bg-slate-100"
+          onClick={() => setInsidePage("patientSchedule")}
         >
-          <button
-            className="fixed right-5 z-40 w-10 rounded-lg border px-1 text-2xl font-light text-slate-400 shadow-md hover:bg-slate-100"
-            onClick={() => setInsidePage("patientSchedule")}
-          >
-            <IoIosReturnLeft />
-          </button>
-          <div className="headingColor relative mt-8 text-center text-3xl font-bold ">
-            SELECT DOCTOR
-          </div>
-          <div className="m-4">
-            <p>FILTER DATE</p>
-            <Calendar
-              placeholder="Select date"
-              className="z-0 w-1/2 rounded-lg border-2 md:w-8/12"
-              value={date}
-              disabledDates={disabledDates.map((e) => new Date(e))}
-              onChange={(e) => {
-                setDate(e.value.toISOString());
-              }}
-              minDate={new Date()}
-              locale="en"
-              dateTemplate={dateTemplate}
+          <IoIosReturnLeft />
+        </button>
+        <div className="headingColor relative mt-8 text-center text-3xl font-bold ">
+          SELECT DOCTOR
+        </div>
+        <div className="m-4">
+          <p>FILTER DATE</p>
+          <Calendar
+            placeholder="Select date"
+            className="z-0 w-1/2 rounded-lg border-2 md:w-8/12"
+            value={date}
+            disabledDates={disabledDates.map((e) => new Date(e))}
+            onChange={(e) => {
+              setDate(e.value.toISOString());
+            }}
+            minDate={new Date()}
+            locale="en"
+            dateTemplate={dateTemplate}
+          />
+        </div>
+
+        {fetching && (
+          <div className="mt-10 flex w-full items-center justify-center">
+            <ProgressSpinner
+              style={{ width: "50px", height: "50px" }}
+              strokeWidth="4"
+              animationDuration="0.5s"
             />
           </div>
+        )}
 
-          {fetching && (
-            <div className="mt-10 flex w-full items-center justify-center">
-              <ProgressSpinner
-                style={{ width: "50px", height: "50px" }}
-                strokeWidth="4"
-                animationDuration="0.5s"
-              />
-            </div>
-          )}
-
-          {doctors.map((doctor) => (
-            <div
-              className="mx-auto my-4 flex w-[90%] cursor-pointer flex-col rounded-lg border-2 border-slate-400 bg-[#F0F3EC] p-4 hover:bg-[#C5E1A5]"
-              onClick={() => {
-                setSelectedDoctor(doctor);
-                setPage("doctorDetail");
-              }}
-            >
-              <div className="flex ">
-                <div className="font-bol w-[60%] text-xl text-[#666CFF]">
-                  {doctor.doctorUUID.firstName}&nbsp;{" "}
-                  {doctor.doctorUUID.lastName}
-                </div>
-                <div className="w-[20%] ">
-                  <Rating
-                    value={
-                      doctor.doctorUUID.reviews.reduce(
-                        (acc, r) => acc + r.score,
-                        0
-                      ) / doctor.doctorUUID.reviews.map((r) => r.score).length
-                    }
-                    start={5}
-                    cancel={false}
-                  />
-                </div>
+        {doctors.map((doctor) => (
+          <div
+            className="mx-auto my-4 flex w-[90%] cursor-pointer flex-col rounded-lg border-2 border-slate-400 bg-[#F0F3EC] p-4 hover:bg-[#C5E1A5]"
+            onClick={() => {
+              setSelectedDoctor(doctor);
+              setPage("doctorDetail");
+            }}
+          >
+            <div className="flex ">
+              <div className="font-bol w-[60%] text-xl text-[#666CFF]">
+                {doctor.doctorUUID.firstName}&nbsp; {doctor.doctorUUID.lastName}
               </div>
-              <div className="text-[#4C4E64]">{doctor.description}</div>
+              <div className="w-[20%] ">
+                <Rating
+                  value={
+                    doctor.doctorUUID.reviews.reduce(
+                      (acc, r) => acc + r.score,
+                      0
+                    ) / doctor.doctorUUID.reviews.map((r) => r.score).length
+                  }
+                  start={5}
+                  cancel={false}
+                />
+              </div>
             </div>
-          ))}
-        </div>
-      {/* )} */}
-      {/* {page === "doctorDetail" && ( */}
+            <div className="text-[#4C4E64]">{doctor.description}</div>
+          </div>
+        ))}
+      </div>
+
       <div
-          className={`mt-4 p-2 opacity-0 duration-200 ${
-            page === "doctorDetail" ? "opacity-100" : "opacity-0 pointer-events-none"
-          } `}
-        >
+        className={`mt-4 p-2 opacity-0 duration-200 ${
+          page === "doctorDetail"
+            ? "opacity-100"
+            : "pointer-events-none opacity-0"
+        } `}
+      >
         <SelectDoctorDetail setPage={setPage} selectedDoctor={selectedDoctor} />
       </div>
-      {/* )} */}
     </>
   );
 };
