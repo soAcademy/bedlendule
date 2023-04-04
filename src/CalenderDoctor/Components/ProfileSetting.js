41033b94393c22323bb232765aa2a9445cfd9eb6
jaiieth <midzty.rb128@gmail.com>
2023-04-04 11:36:32 +0700

    feat: add upload photo

--- src/CalenderDoctor/Components/ProfileSetting.js
+++ src/CalenderDoctor/Components/ProfileSetting.js
@@ -1,138 +1,190 @@
-import { classNames } from "primereact/utils";
+import axios from "axios";
 import { useState, useEffect } from "react";
-import { CiCircleRemove } from "react-icons/ci";
-import { IoIosCloseCircle } from "react-icons/io";
+import { BiEditAlt } from "react-icons/bi";
+import { enforceFormat, formatToPhone } from "../Functions/validateForm";
 
 const ProfileSetting = () => {
-  const [username, setUsername] = useState();
   const [email, setEmail] = useState();
   const [licenseId, setLicenseId] = useState();
   const [contact, setContact] = useState();
-  const [selectedImage, setSelectedImage] = useState(null);
+  const [background, setBackground] = useState();
+  const [file, setFile] = useState(null);
+  const userProfile = JSON.parse(localStorage.getItem("userprofile"));
+  const [selectedImage, setSelectedImage] = useState(
+    userProfile?.profilePictureUrl
+  );
+  const token = localStorage.getItem("access-token");
 
-  const getUserUUID = () => localStorage.getItem("uuid");
+  useEffect(() => {
+    setSelectedImage(userProfile?.profilePictureUrl);
+    setEmail(userProfile?.email);
+    setLicenseId(userProfile?.licenseId);
+    setContact(userProfile?.phoneNumber);
+    setBackground(userProfile?.background);
+  }, [userProfile]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const _data = JSON.stringify({
-      uuid: getUserUUID(),
-      email: email,
-      phoneNumber: contact,
-      licenseId: licenseId,
-      profilePictureUrl: selectedImage.name,
-    });
-    console.log("_data", _data);
+  const handleFileChange = async (event) => {
+    const selectedFile = event.target.files[0];
+    setFile(selectedFile);
+    const temporaryUrl = URL.createObjectURL(selectedFile);
+    setSelectedImage(temporaryUrl);
   };
-  // console.log("selectedImage", selectedImage);
+
+  const updateProfile = (input, imageUrl) => {
+    const data = JSON.stringify({
+      email: input["email"].value,
+      phoneNumber: input["phoneNumber"].value,
+      licenseId: input["licenseId"].value,
+      background: input["background"].value,
+      profilePictureUrl: imageUrl,
+    });
+
+    const config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: "http://localhost:5555/bedlendule/updateUser",
+      headers: {
+        authorization: token,
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
+    axios
+      .request(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleSubmit = async (event, input) => {
+    event.preventDefault();
+    if (file) {
+      const formData = new FormData();
+      formData.append("image", file);
+      const config = {
+        method: "post",
+        url: "http://localhost:5555/uploadImg",
+        headers: {
+          authorization: token,
+          "Content-Type": "multipart/form-data",
+        },
+        data: formData,
+      };
+      axios(config)
+        .then(function (response) {
+          updateProfile(input, response.data);
+        })
+        .catch(function (error) {});
+    } else {
+      updateProfile(input, userProfile.profilePictureUrl);
+    }
+  };
+
   return (
-    <>
-      <div>
-        {" "}
-        <form onSubmit={handleSubmit}>
-          <div className="my-auto flex h-screen flex-col items-center gap-y-2 ">
-            <div className=" flex h-full w-full flex-col  ">
-              <div className="mx-auto my-auto w-1/2 ">
-                <h1 className="headingColor my-4 text-center font-kanit text-2xl font-bold">
-                  Profile setting
-                </h1>
-                <div className="mx-auto h-[220px] w-[220px] overflow-hidden rounded-full object-contain">
-                  {selectedImage && (
-                    <div className="relative mx-auto my-auto ">
-                      <img
-                        alt="not found"
-                        width={"220px"}
-                        height={"220px"}
-                        src={URL.createObjectURL(selectedImage)}
-                        className=" mx-auto aspect-square"
-                      />
-                    </div>
-                  )}
-                  {selectedImage === null && (
-                    <div className="relative mx-auto my-auto ">
-                      <img
-                        alt="not found"
-                        width={"220px"}
-                        height={"220px"}
-                        src="https://www.concilioexpert.com/wp-content/uploads/2017/12/blank-profile-picture-973460-1024x1024.png"
-                        className=" mx-auto aspect-square"
-                      />
-                    </div>
-                  )}
-                </div>
-
-                <input
-                  type="file"
-                  name="myImage"
-                  onChange={(event) => {
-                    console.log("picture file", event.target.files[0]);
-                    setSelectedImage(event.target.files[0]);
-                  }}
-                  className="text-light mb-5 text-sm"
-                />
-                <button
-                  className="absolute  top-[45%] right-[30%] text-xl md:right-[40%] "
-                  onClick={() => setSelectedImage(null)}
-                >
-                  <IoIosCloseCircle className="text-red-500" />
-                </button>
-                <div className="my-auto flex flex-col space-y-2  ">
-                  <div className="flex flex-row space-x-5">
-                    <div className="my-auto w-1/3">Username</div>
-                    <input
-                      onChange={(e) => setUsername(e.target.value)}
-                      type="text"
-                      className="flex h-[45px] w-3/5 items-center rounded-lg border-2 border-slate-500 p-2"
-                      id="username"
-                      placeholder="@Username"
-                    />
-                  </div>
-                  <div className="flex flex-row space-x-5">
-                    <div className="my-auto w-1/3">Email</div>
-                    <input
-                      onChange={(e) => setEmail(e.target.value)}
-                      type="email"
-                      className="flex h-[45px] w-3/5 items-center rounded-lg border-2 border-slate-500 p-2"
-                      id="email"
-                      placeholder="example@email.com"
-                    />
-                  </div>
-                  <div className="flex flex-row space-x-5">
-                    <div className="my-auto w-1/3">License ID</div>
-                    <input
-                      onChange={(e) => setLicenseId(e.target.value)}
-                      type="text"
-                      className="flex h-[45px] w-3/5 items-center rounded-lg border-2 border-slate-500 p-2"
-                      id="licenseId"
-                      placeholder="LIC-xxxxxxxx"
-                    />
-                  </div>
-                  <div className="flex flex-row space-x-5">
-                    <div className="my-auto w-1/3">Contact</div>
-                    <input
-                      onChange={(e) => setContact(e.target.value)}
-                      type="tel"
-                      className="flex h-[45px] w-3/5 items-center rounded-lg border-2 border-slate-500 p-2"
-                      id="phoneNumber"
-                      placeholder="099-xxx-xxxx"
-                      minLength={10}
-                      maxlength={10}
-                    />
-                  </div>
-
-                  <button
-                    className="button mx-10 my-[10px] py-2 "
-                    type="submit"
-                  >
-                    Submit
-                  </button>
-                </div>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        const input = e.target;
+        handleSubmit(e, input);
+      }}
+      className="z-10 mt-10"
+    >
+      <div className="flex flex-col items-center gap-y-2 ">
+        <h1 className="headingColor my-4 text-center font-kanit text-2xl font-bold">
+          Profile setting
+        </h1>
+        <div className="relative h-48 w-48 overflow-hidden rounded-full duration-150 hover:opacity-90">
+          {selectedImage && (
+            <label for="file-input">
+              <img
+                alt="not found"
+                src={selectedImage}
+                className="mx-auto h-48 w-48 cursor-pointer rounded-full object-cover hover:opacity-80"
+              />
+              <div className="absolute top-0 right-0 h-48 w-48 cursor-pointer rounded-full text-2xl text-slate-500 opacity-0 duration-150 hover:opacity-90">
+                <BiEditAlt className="absolute top-7 right-7" />
               </div>
-            </div>
+            </label>
+          )}
+          {selectedImage === null && (
+            <label for="file-input">
+              <img
+                alt="not found"
+                src="https://www.concilioexpert.com/wp-content/uploads/2017/12/blank-profile-picture-973460-1024x1024.png"
+                className="mx-auto object-cover"
+              />
+              <div className="absolute top-0 right-0 h-48 w-48 cursor-pointer rounded-full text-2xl text-slate-500 opacity-0 duration-150 hover:opacity-90">
+                <BiEditAlt className="absolute top-7 right-7" />
+              </div>
+            </label>
+          )}
+          <input
+            id="file-input"
+            type="file"
+            name="myImage"
+            onChange={handleFileChange}
+            className="hidden"
+          />
+        </div>
+        <div className="mt-10 flex flex-col space-y-4">
+          <div className="flex flex-row space-x-5">
+            <div className="my-auto w-1/3">Email</div>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              className="flex h-[45px] w-3/5 items-center rounded-lg border-2 border-slate-500 p-2"
+              id="email"
+              placeholder="example@email.com"
+              defaultValue={email}
+            />
           </div>
-        </form>
-        <div></div>
+          <div className="flex flex-row space-x-5">
+            <div className="my-auto w-1/3">License ID</div>
+            <input
+              onChange={(e) => setLicenseId(e.target.value)}
+              type="text"
+              className="flex h-[45px] w-3/5 items-center rounded-lg border-2 border-slate-500 p-2"
+              id="licenseId"
+              placeholder="LIC-xxxxxxxx"
+              defaultValue={licenseId}
+            />
+          </div>
+          <div className="flex flex-row space-x-5">
+            <div className="my-auto w-1/3">Contact</div>
+            <input
+              type="tel"
+              className="flex h-[45px] w-3/5 items-center rounded-lg border-2 border-slate-500 p-2"
+              id="phoneNumber"
+              name="phoneNumber"
+              placeholder="xxx-xxx-xxxx"
+              maxLength={16}
+              minLength={16}
+              onKeyDown={enforceFormat}
+              onKeyUp={formatToPhone}
+              defaultValue={contact}
+            />
+          </div>
+          <label for="background" className="mx-auto">
+            Background
+          </label>
+          <textarea
+            onChange={(e) => {}}
+            className="flex min-h-[120px] items-center rounded-lg border-2 border-slate-500 p-2"
+            maxLength={130}
+            name="background"
+            id="background"
+            placeholder="Background"
+            defaultValue={background}
+          />
+          <button className="button mx-auto w-1/2 py-2 " type="submit">
+            Submit
+          </button>
+        </div>
       </div>
-    </>
+    </form>
   );
 };
 export default ProfileSetting;
